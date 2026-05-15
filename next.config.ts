ac23a293efbc42a69a5331091014f027d6b0a91c
igonzalez <igonzalez@st.com.uy>
2026-05-15 13:56:30 -0300

    fix: ajustar rutas para dominio custom

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === 'production' ? '/trick' : '',
 };
 
 export default nextConfig;
